813f23ce68253cfca31067afdcc024ba4b055f73
DestinyDeCastro <52354375+DestinyDeCastro@users.noreply.github.com>
2023-10-27 12:51:03 +0200

    Update stop_server.sh

--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -1,19 +1,5 @@
 #!/bin/bash
-
-# Verificar si el servicio Apache está en ejecución
-if ! systemctl is-active --quiet httpd; then
-    echo "El servicio Apache ya está detenido."
-else
-    # Detener el servicio Apache
-    sudo systemctl stop httpd
-
-    # Comprobar si la operación de detención tuvo éxito
-    if [ $? -eq 0 ]; then
-        echo "El servicio Apache se ha detenido correctamente."
-    else
-        echo "Error al detener el servicio Apache."
-        exit 1
-    fi
+isExistApp = `pgrep httpd`
+if [[ -n  $isExistApp ]]; then
+    service httpd stop        
 fi
-
-exit 0
