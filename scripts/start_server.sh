0faf2f729e6df40850433a3f667d458412fa1081
DestinyDeCastro <52354375+DestinyDeCastro@users.noreply.github.com>
2023-10-27 12:51:40 +0200

    Update start_server.sh

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
-# Iniciar el servicio de Apache
-systemctl start httpd
+# Iniciar el servicio de Apache2
+sudo systemctl start apache2
+
+# Verificar si Apache2 se inició correctamente
+if [ $? -eq 0 ]; then
+  echo "El servicio de Apache2 se ha iniciado correctamente."
+else
+  echo "Error: No se pudo iniciar el servicio de Apache2."
+fi
