13d7f57621fd827cfdde654c6119a4616a734a78
DestinyDeCastro <52354375+DestinyDeCastro@users.noreply.github.com>
2023-10-27 12:38:58 +0200

    Update install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,4 +1,48 @@
 #!/bin/bash
 
-if ! [ -x "$(command -v httpd)" ]; then yum install -y httpd24 >&2;   exit 1; fi # install apache if not already installed
-if ! [ -x "$(command -v php)" ]; then yum install php72 php72-mysqlnd php72-imap php72-pecl-memcache php72-pecl-apcu php72-gd php72-mbstring -y >&2;   exit 1; fi # install PHP
+# Check if Apache is installed
+if ! [ -x "$(command -v httpd)" ]; then
+    echo "Apache (httpd) is not installed. Installing..."
+    
+    # Adjust the package manager based on your distribution
+    if [ -x "$(command -v apt-get)" ]; then
+        sudo apt-get install -y apache2
+    elif [ -x "$(command -v yum)" ]; then
+        sudo yum install -y httpd
+    else
+        echo "Unsupported package manager. Please install Apache manually."
+        exit 1
+    fi
+    
+    if [ $? -ne 0 ]; then
+        echo "Failed to install Apache. Exiting."
+        exit 1
+    fi
+    
+    echo "Apache installed successfully."
+fi
+
+# Check if PHP is installed
+if ! [ -x "$(command -v php)" ]; then
+    echo "PHP is not installed. Installing..."
+
+    # Adjust the package manager based on your distribution
+    if [ -x "$(command -v apt-get)" ]; then
+        sudo apt-get install -y php
+    elif [ -x "$(command -v yum)" ]; then
+        sudo yum install -y php
+    else
+        echo "Unsupported package manager. Please install PHP manually."
+        exit 1
+    fi
+
+    if [ $? -ne 0 ]; then
+        echo "Failed to install PHP. Exiting."
+        exit 1
+    fi
+
+    echo "PHP installed successfully."
+fi
+
+echo "All required packages are installed."
+exit 0
