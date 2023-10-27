afb7a6f23cfad3648f3a417bc5c9318c64d155d5
DestinyDeCastro <52354375+DestinyDeCastro@users.noreply.github.com>
2023-10-27 14:54:00 +0200

    Update stop_server.sh

--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -1,2 +1,23 @@
 #!/bin/bash
-service httpd stop
+
+# Comprueba si el servidor está en ejecución
+if [ -f "server.pid" ]; then
+    # Lee el ID del proceso del archivo "server.pid"
+    pid=$(cat "server.pid")
+
+    # Comprueba si el proceso está en ejecución
+    if ps -p $pid > /dev/null; then
+        echo "Deteniendo el servidor con el ID de proceso $pid..."
+        # Detén el servidor
+        kill $pid
+
+        # Elimina el archivo "server.pid"
+        rm "server.pid"
+
+        echo "Servidor detenido con éxito."
+    else
+        echo "El servidor no está en ejecución."
+    fi
+else
+    echo "El servidor no está en ejecución."
+fi
