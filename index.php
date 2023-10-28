ea1d04a714d730681aa49c4e11f005564b20a86e
DestinyDeCastro <52354375+DestinyDeCastro@users.noreply.github.com>
2023-10-28 13:14:23 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,17 +1,31 @@
-<?php
-// Cargar las dependencias (asegúrate de que composer install se haya ejecutado)
-require 'vendor/autoload.php';
-
-use Carbon\Carbon;
-
-try {
-    // Obtener la fecha y hora actual con Carbon
-    $date = Carbon::now();
-
-    // Mostrar la fecha y hora actual en un formato legible
-    echo "Current Date and Time: " . $date->toDayDateTimeString();
-} catch (Exception $e) {
-    // Manejar cualquier error que pueda ocurrir al usar Carbon
-    echo "Error: " . $e->getMessage();
-}
-?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Hora Actual</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #f4f4f4;
+            text-align: center;
+        }
+        h1 {
+            color: #333;
+        }
+        p {
+            font-size: 20px;
+            color: #0078d4;
+        }
+    </style>
+</head>
+<body>
+    <h1>Hora Actual</h1>
+    <?php
+    try {
+        $date = new DateTime();
+        echo "<p>La hora actual es: " . $date->format('Y-m-d H:i:s') . "</p>";
+    } catch (Exception $e) {
+        echo "<p>Error: " . $e->getMessage() . "</p>";
+    }
+    ?>
+</body>
+</html>
